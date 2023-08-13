447549a2aa2dacba1abc38dc022c27f293899e74
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-13 14:00:25 +0530

    add:  imports for components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as MovieCard } from "./MovieCard";
+export { default as Navbar } from "./Navbar";
+export { GenreFilter } from "./Filters";
+export { ReleaseYearFilter } from "./Filters";
+export { RatingFilter } from "./Filters";
