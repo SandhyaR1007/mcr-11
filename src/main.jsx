2fcd69f6257627299ceb61ae1e72da3043916e92
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-13 14:00:48 +0530

    update: added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { MoviesProvider } from "./context/MoviesContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MoviesProvider>
+      <Router>
+        <App />
+      </Router>
+    </MoviesProvider>
+  </React.StrictMode>
+);
