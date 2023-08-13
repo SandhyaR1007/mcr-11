382dc674e737876ee4072896b776ab75564ace04
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-13 14:01:32 +0530

    add: imports for pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as MovieListing } from "./MovieListing";
+export { default as MovieDetails } from "./MovieDetails";
+export { default as StarredMovies } from "./StarredMovies";
+export { default as Watchlist } from "./Watchlist";
